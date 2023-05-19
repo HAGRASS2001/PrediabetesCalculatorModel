7d0db9164825ae854622a72218db82a0ebb661f0
HAGRASS2001 <83500189+HAGRASS2001@users.noreply.github.com>
2023-05-19 21:57:57 +0300

    new dep commit

--- mlapi.py
+++ mlapi.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import pickle
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 app = FastAPI()
 
@@ -9,17 +10,65 @@ class predictionData(BaseModel):
     Pregnancies: int
     Glucose: int
     BloodPressure: int
-    SkinThickness: int
-    Insulin: int
-    BMI: float
+    SkinThickness: float
+    Insulin: float
+    BMI: int
     DiabetesPedigreeFunction: float
     Age: int
 
-with open('svm_model2.pkl', 'rb') as f:
+with open('New_svm_model.pkl', 'rb') as f:
     model = pickle.load(f)
+
+with open('scaler.pkl', 'rb') as f:
+    scaler = pickle.load(f)
 
 @app.post('/')
 async def prediabetes_endPoint(data:predictionData):
+    if data.Age <= 30:
+        data.Age = 1
+    elif data.Age <= 40:
+        data.Age = 2
+    elif data.Age <= 50:
+        data.Age = 3
+    elif data.Age <= 60:
+        data.Age = 4
+    else:
+        data.Age = 5
+
+    if data.Glucose <= 60:
+        data.Glucose = 1
+    elif data.Glucose <= 80:
+        data.Glucose = 2
+    elif data.Glucose <= 140:
+        data.Glucose = 3
+    elif data.Glucose <= 180:
+        data.Glucose = 4
+    else:
+        data.Glucose = 5
+
+    if data.BloodPressure <= 60:
+        data.BloodPressure = 1
+    elif data.BloodPressure <= 75:
+        data.BloodPressure = 2
+    elif data.BloodPressure <= 90:
+        data.BloodPressure = 3
+    elif data.BloodPressure <= 100:
+        data.BloodPressure = 4
+    else:
+        data.BloodPressure = 5
+
+    if data.BMI <= 19:
+        data.BMI = 1
+    elif data.BMI <= 24:
+        data.BMI = 2
+    elif data.BMI <= 30:
+        data.BMI = 3
+    elif data.BMI <= 40:
+        data.BMI = 4
+    else:
+        data.BMI = 5
+
     df = pd.DataFrame([data.dict().values()], columns = data.dict().keys())
-    yhat = model.predict(df)
+    newDF = scaler.transform(df)
+    yhat = model.predict(newDF)
     return int(yhat)
